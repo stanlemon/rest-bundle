149fe7e80036e04df5c29a93864c24eb0d926332
Stan Lemon <stosh1985@gmail.com>
2014-10-01 21:15:33 -0400

    Adding registry unit test

---
+++ Tests/Object/RegistryTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Lemon\RestBundle\Tests\Object;
+
+use Lemon\RestBundle\Object\Registry;
+
+/**
+ * @coversDefaultClass \Lemon\RestBundle\Object\Registry
+ */
+class RegistryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::addClass()
+     * @covers ::hasClass()
+     * @covers ::getClass()
+     * @covers ::getClasses()
+     */
+    public function testAddClass()
+    {
+        $registry = new Registry();
+        $registry->addClass('person', '\Lemon\RestBundle\Tests\Fixtures\Person');
+
+        $this->assertTrue($registry->hasClass('person'));
+        $this->assertEquals(
+            '\Lemon\RestBundle\Tests\Fixtures\Person',
+            $registry->getClass('person')
+        );
+        $this->assertEquals(
+            array(
+                'person' => '\Lemon\RestBundle\Tests\Fixtures\Person'
+            ),
+            $registry->getClasses()
+        );
+    }
+
+    /**
+     * @covers ::addClass()
+     */
+    public function testAddClassDoesNotExist()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+
+        $registry = new Registry();
+        $registry->addClass('foo', '\foo\bar');
+    }
+}
