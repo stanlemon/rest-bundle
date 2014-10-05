aae4131a1069f960367bfbb8e837ae2649eb67ba
Stan Lemon <stosh1985@gmail.com>
2014-10-04 23:45:10 -0400

    Add type hint

--- Event/PreSearchEvent.php
+++ Event/PreSearchEvent.php
@@ -1,13 +1,14 @@
 <?php
 namespace Lemon\RestBundle\Event;
 
+use Lemon\RestBundle\Object\Criteria;
 use Symfony\Component\EventDispatcher\Event;
 
 class PreSearchEvent extends Event
 {
     protected $criteria;
 
-    public function __construct($criteria)
+    public function __construct(Criteria $criteria)
     {
         $this->criteria = $criteria;
     }
