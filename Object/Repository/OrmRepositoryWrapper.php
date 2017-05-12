2695698e0cc096ad60f996b4e4599348fb24c9a9
Olivier Maisonneuve <olivierm@ex-situ.com>
2017-05-12 17:54:54 -0400

    OrmRepositoryWrapper::buildWhereClause() updated to proceed to like on string fields and detect foreign id field.

--- Object/Repository/OrmRepositoryWrapper.php
+++ Object/Repository/OrmRepositoryWrapper.php
@@ -13,7 +13,7 @@ class OrmRepositoryWrapper implements Repository
 	 * @var ObjectRepository
 	 */
 	protected $repository;
-	
+
 	/**
 	 * @var ClassMetadata
 	 */
@@ -29,8 +29,8 @@ class OrmRepositoryWrapper implements Repository
 		$reflection = new \ReflectionObject($repository);
 		$property = $reflection->getProperty('_class');
 		$property->setAccessible(true);
-		
-		$this->metadata = $property->getValue($repository); 
+
+		$this->metadata = $property->getValue($repository);
 	}
 
 	/**
@@ -56,15 +56,15 @@ class OrmRepositoryWrapper implements Repository
 	public function search(Criteria $criteria)
 	{
 		$qb = $this->repository->createQueryBuilder('e');
-	
+
 		$this->buildWhereClause($qb, $criteria);
-	
+
 		$qb->select();
-		
+
 		if ($criteria->getOrderBy()) {
     	   $qb->orderBy('e.' . $criteria->getOrderBy(), $criteria->getOrderDir());
 		}
-		
+
 		if ($criteria->getOffset()) {
 		   $qb->setFirstResult($criteria->getOffset());
 		}
@@ -72,28 +72,44 @@ class OrmRepositoryWrapper implements Repository
 		if ($criteria->getLimit()) {
 		   $qb->setMaxResults($criteria->getLimit());
 		}
-		
+
 		return $qb->getQuery()
 		   ->execute();
 	}
 
-	/**
-	 * @param QueryBuilder $qb
-	 * @param Criteria $criteria
-	 */
-	protected function buildWhereClause(QueryBuilder $qb, Criteria $criteria)
-	{
-		$values = array();
+    /**
+     * @param QueryBuilder $qb
+     * @param Criteria     $criteria
+     */
+    protected function buildWhereClause(QueryBuilder $qb, Criteria $criteria)
+    {
+        foreach ($criteria as $field => $value) {
+            $value = trim($value);
 
-        foreach ($criteria as $key => $value) {
-            if ($this->metadata->hasField($key) || $this->metadata->hasAssociation($key)) {
-				$qb->andWhere('e.' . $key . ' = :' . $key);
-				$values[$key] = $value;
-			}
+            if (empty($value) === false) {
+                $fields = [$field];
+
+                if (strpos($field, '_id') !== false) {
+                    $fields[] = str_replace('_id', '', $field);
+                }
+
+                foreach ($fields as $fieldName) {
+                    if (
+                        $this->metadata->hasField($fieldName)
+                        || $this->metadata->hasAssociation($fieldName)
+                    ) {
+                        if ($this->metadata->getTypeOfField($fieldName) === 'string') {
+                            $qb->andWhere('e.' . $fieldName . ' LIKE :' . $fieldName);
+                            $qb->setParameter($fieldName, sprintf('%%%s%%', $value));
+                        } else {
+                            $qb->andWhere('e.' . $fieldName . ' = :' . $fieldName);
+                            $qb->setParameter($fieldName, $value);
+                        }
+                    }
+                }
+            }
         }
-		
-		$qb->setParameters($values);
-	}
+    }
 
 	/**
 	 * @param int $id
